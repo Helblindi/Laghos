98273195b3394103d54a63069416e885c64db7d0
Madison Sheridan <madison.sheridan94@gmail.com>
2025-02-17 17:37:44 -0600

    Correctly importing the Laglos library it seems. I just wish there were a way to automatically find Boost and Hiop libraries when adding Laglos library since they are Laglos dependencies.

---
+++ config/defaults.cmake
@@ -0,0 +1,8 @@
+set(MFEM_DIR ${CMAKE_CURRENT_SOURCE_DIR}/../mfem/)
+set(mfem_DIR ${CMAKE_CURRENT_SOURCE_DIR}/../mfem/)
+
+set(LAGLOS_DIR ${CMAKE_CURRENT_SOURCE_DIR}/../Laglos/)
+
+# set(LAGLOS_HIOP_DIR ${CMAKE_CURRENT_SOURCE_DIR}/../hiop/)
+
+# option(LAGLOS_USE_HIOP "Utilize sparse linear algebra" ON)
