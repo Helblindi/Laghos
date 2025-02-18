c5536328f7647836a25ad95461fd4a7fa0d8fd7b
Madison Sheridan <madison.sheridan94@gmail.com>
2025-02-18 11:05:37 -0600

    Adjusted paths mfem_DIR, MFEM_DIR, laglos_DIR, LAGLOS_DIR to point to relative install and config files. Added option in configuration file to use low order solver to provide IDP to Laghos.

--- config/defaults.cmake
+++ config/defaults.cmake
@@ -1,9 +1,12 @@
-set(MFEM_DIR ${CMAKE_CURRENT_SOURCE_DIR}/../mfem-git/build/install/lib/cmake/mfem
-    CACHE PATH "Path to MFEMConfig.cmake configuration file")
+set(MFEM_DIR ${CMAKE_CURRENT_SOURCE_DIR}/../mfem/build/install
+    CACHE PATH "absolute path to the MFEM build or install prefix")
+set(mfem_DIR ${CMAKE_CURRENT_SOURCE_DIR}/../mfem
+    CACHE PATH "absolute path to where MFEMConfig.cmake is")
 
-set(LAGLOS_DIR ${CMAKE_CURRENT_SOURCE_DIR}/../Laglos/
-    CACHE PATH "Path to LAGLOSConfig.cmake configuration file")
-
-# set(LAGLOS_HIOP_DIR ${CMAKE_CURRENT_SOURCE_DIR}/../hiop/)
+set(LAGLOS_DIR ${CMAKE_CURRENT_SOURCE_DIR}/../Laglos/build/install
+    CACHE PATH "absolute path to the LAGLOS build or install prefix")
+set(laglos_DIR ${CMAKE_CURRENT_SOURCE_DIR}/../Laglos/build/install/lib/cmake
+    CACHE PATH "absolute path to where LAGLOSConfig.cmake is")
 
 # option(LAGLOS_USE_HIOP "Utilize sparse linear algebra" ON)
+option(LAGHOS_LIMIT "Utilize low order Lagrangian solution to provide IDP baseline" ON)
