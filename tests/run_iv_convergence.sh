e45fe7daa8594671778e5ba7424b86678faa083c
Madison Sheridan <madison.sheridan94@gmail.com>
2025-05-19 21:03:37 -0500

    Adding scripts to quickly run convergence tests for various problems.

--- tests/run_iv_convergence.sh
+++ tests/run_iv_convergence.sh
@@ -10,30 +10,34 @@ results_dir="${bin_dir}/results"
 cd $bin_dir
 
 # EDIT THESE PARAMS
-final_time=0.2
+mesh="${laghos_dir}/../Laglos/data/square-vortex.mesh"
+final_time=1.
 cfl=0.5
 ot=1
 ok=2 # must be ot + 1
 oq=4 # must be >= 4
 solver_type=14
-output_location="${results_dir}/tests/ot1/sod"
-output_file="${output_location}/out-sod-r"
+output_location="${results_dir}/tests/ot1/iv"
+output_file="${output_location}/out-iv-r"
 ########
 
-options="-dim 1 -fa -p 2 -tf ${final_time} -cfl ${cfl} "
+options="-m ${mesh} -fa -p 9 -tf ${final_time} -cfl ${cfl} "
 options+="-ot ${ot} -ok ${ok} -oq ${oq} -k ${output_location} "
 options+="-s ${solver_type} -idp "
 
 # echo $options
 
-./laghos ${options} -rs 3 > ${output_file}3 &
+./laghos ${options} -rs 0 > ${output_file}0 &
+./laghos ${options} -rs 1 > ${output_file}1 &
+./laghos ${options} -rs 2 > ${output_file}2 &
+./laghos ${options} -rs 3 > ${output_file}3 &   
 ./laghos ${options} -rs 4 > ${output_file}4 &
 ./laghos ${options} -rs 5 > ${output_file}5 &
-./laghos ${options} -rs 6 > ${output_file}6 &
-./laghos ${options} -rs 7 > ${output_file}7 &
-./laghos ${options} -rs 8 > ${output_file}8 &
-./laghos ${options} -rs 9 > ${output_file}9 &
-./laghos ${options} -rs 10 > ${output_file}10 &
+# ./laghos ${options} -rs 6 > ${output_file}6 &
+# ./laghos ${options} -rs 7 > ${output_file}7 &
+# ./laghos ${options} -rs 8 > ${output_file}8 &
+# ./laghos ${options} -rs 9 > ${output_file}9 &
+# ./laghos ${options} -rs 10 > ${output_file}10 &
 
 echo "Run convergence with command:"
 echo "python3 ${create_convergence} ${output_location}"
