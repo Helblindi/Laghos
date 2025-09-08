0e51c69e161d7d06728f89dff0dd0508408f1192
Madison Sheridan <madison.sheridan94@gmail.com>
2025-09-08 11:02:15 -0500

    Removing initial time functions as they are unused.

--- laghos.cpp
+++ laghos.cpp
@@ -77,10 +77,10 @@ using namespace mfem;
 static int problem, dim;
 
 // Forward declarations.
-double e0(const Vector &);
-double rho0(const Vector &);
-double gamma_func(const Vector &);
-void v0(const Vector &, Vector &);
+// double e0(const Vector &);
+// double rho0(const Vector &);
+// double gamma_func(const Vector &);
+// void v0(const Vector &, Vector &);
 void MassesAndVolumesAtPosition(const ParGridFunction &u, const GridFunction &x, Vector &el_mass, Vector &el_vol);
 
 static long GetMaxRssMB();
@@ -1704,190 +1704,190 @@ int main(int argc, char *argv[])
 }
 
 
-double rho0(const Vector &x)
-{
-   switch (problem)
-   {
-      case 0: return 1.0;
-      case 1: return 1.0;
-      case 2: return (x(0) < 0.5) ? 1.0 : 0.1;
-      case 3: return (dim == 2) ? (x(0) > 1.0 && x(1) > 1.5) ? 0.125 : 1.0
-                        : x(0) > 1.0 && ((x(1) < 1.5 && x(2) < 1.5) ||
-                                         (x(1) > 1.5 && x(2) > 1.5)) ? 0.125 : 1.0;
-      case 4: return 1.0;
-      case 5:
-      {
-         if (x(0) >= 0.5 && x(1) >= 0.5) { return 0.5313; }
-         if (x(0) <  0.5 && x(1) <  0.5) { return 0.8; }
-         return 1.0;
-      }
-      case 6:
-      {
-         if (x(0) <  0.5 && x(1) >= 0.5) { return 2.0; }
-         if (x(0) >= 0.5 && x(1) <  0.5) { return 3.0; }
-         return 1.0;
-      }
-      case 7: return x(1) >= 0.0 ? 2.0 : 1.0;
-      default: MFEM_ABORT("Bad number given for problem id!"); return 0.0;
-   }
-}
+// double rho0(const Vector &x)
+// {
+//    switch (problem)
+//    {
+//       case 0: return 1.0;
+//       case 1: return 1.0;
+//       case 2: return (x(0) < 0.5) ? 1.0 : 0.1;
+//       case 3: return (dim == 2) ? (x(0) > 1.0 && x(1) > 1.5) ? 0.125 : 1.0
+//                         : x(0) > 1.0 && ((x(1) < 1.5 && x(2) < 1.5) ||
+//                                          (x(1) > 1.5 && x(2) > 1.5)) ? 0.125 : 1.0;
+//       case 4: return 1.0;
+//       case 5:
+//       {
+//          if (x(0) >= 0.5 && x(1) >= 0.5) { return 0.5313; }
+//          if (x(0) <  0.5 && x(1) <  0.5) { return 0.8; }
+//          return 1.0;
+//       }
+//       case 6:
+//       {
+//          if (x(0) <  0.5 && x(1) >= 0.5) { return 2.0; }
+//          if (x(0) >= 0.5 && x(1) <  0.5) { return 3.0; }
+//          return 1.0;
+//       }
+//       case 7: return x(1) >= 0.0 ? 2.0 : 1.0;
+//       default: MFEM_ABORT("Bad number given for problem id!"); return 0.0;
+//    }
+// }
 
-double gamma_func(const Vector &x)
-{
-   switch (problem)
-   {
-      case 0: return 5.0 / 3.0;
-      case 1: return 1.4;
-      case 2: return 1.4;
-      case 3:
-         if (dim == 1) { return (x(0) > 0.5) ? 1.4 : 1.5; }
-         else { return (x(0) > 1.0 && x(1) <= 1.5) ? 1.4 : 1.5; }
-      case 4: return 5.0 / 3.0;
-      case 5: return 1.4;
-      case 6: return 1.4;
-      case 7: return 5.0 / 3.0;
-      default: MFEM_ABORT("Bad number given for problem id!"); return 0.0;
-   }
-}
+// double gamma_func(const Vector &x)
+// {
+//    switch (problem)
+//    {
+//       case 0: return 5.0 / 3.0;
+//       case 1: return 1.4;
+//       case 2: return 1.4;
+//       case 3:
+//          if (dim == 1) { return (x(0) > 0.5) ? 1.4 : 1.5; }
+//          else { return (x(0) > 1.0 && x(1) <= 1.5) ? 1.4 : 1.5; }
+//       case 4: return 5.0 / 3.0;
+//       case 5: return 1.4;
+//       case 6: return 1.4;
+//       case 7: return 5.0 / 3.0;
+//       default: MFEM_ABORT("Bad number given for problem id!"); return 0.0;
+//    }
+// }
 
 
 static double rad(double x, double y) { return sqrt(x*x + y*y); }
 
 
-void v0(const Vector &x, Vector &v)
-{
-   const double atn = dim!=1 ? pow((x(0)*(1.0-x(0))*4*x(1)*(1.0-x(1))*4.0),
-                                   0.4) : 0.0;
-   switch (problem)
-   {
-      case 0:
-         v(0) =  sin(M_PI*x(0)) * cos(M_PI*x(1));
-         v(1) = -cos(M_PI*x(0)) * sin(M_PI*x(1));
-         if (x.Size() == 3)
-         {
-            v(0) *= cos(M_PI*x(2));
-            v(1) *= cos(M_PI*x(2));
-            v(2) = 0.0;
-         }
-         break;
-      case 1: v = 0.0; break;
-      case 2: v = 0.0; break;
-      case 3: v = 0.0; break;
-      case 4:
-      {
-         v = 0.0;
-         const double r = rad(x(0), x(1));
-         if (r < 0.2)
-         {
-            v(0) =  5.0 * x(1);
-            v(1) = -5.0 * x(0);
-         }
-         else if (r < 0.4)
-         {
-            v(0) =  2.0 * x(1) / r - 5.0 * x(1);
-            v(1) = -2.0 * x(0) / r + 5.0 * x(0);
-         }
-         else { }
-         break;
-      }
-      case 5:
-      {
-         v = 0.0;
-         if (x(0) >= 0.5 && x(1) >= 0.5) { v(0)=0.0*atn, v(1)=0.0*atn; return;}
-         if (x(0) <  0.5 && x(1) >= 0.5) { v(0)=0.7276*atn, v(1)=0.0*atn; return;}
-         if (x(0) <  0.5 && x(1) <  0.5) { v(0)=0.0*atn, v(1)=0.0*atn; return;}
-         if (x(0) >= 0.5 && x(1) <  0.5) { v(0)=0.0*atn, v(1)=0.7276*atn; return; }
-         MFEM_ABORT("Error in problem 5!");
-         return;
-      }
-      case 6:
-      {
-         v = 0.0;
-         if (x(0) >= 0.5 && x(1) >= 0.5) { v(0)=+0.75*atn, v(1)=-0.5*atn; return;}
-         if (x(0) <  0.5 && x(1) >= 0.5) { v(0)=+0.75*atn, v(1)=+0.5*atn; return;}
-         if (x(0) <  0.5 && x(1) <  0.5) { v(0)=-0.75*atn, v(1)=+0.5*atn; return;}
-         if (x(0) >= 0.5 && x(1) <  0.5) { v(0)=-0.75*atn, v(1)=-0.5*atn; return;}
-         MFEM_ABORT("Error in problem 6!");
-         return;
-      }
-      case 7:
-      {
-         v = 0.0;
-         v(1) = 0.02 * exp(-2*M_PI*x(1)*x(1)) * cos(2*M_PI*x(0));
-         break;
-      }
-      default: MFEM_ABORT("Bad number given for problem id!");
-   }
-}
+// void v0(const Vector &x, Vector &v)
+// {
+//    const double atn = dim!=1 ? pow((x(0)*(1.0-x(0))*4*x(1)*(1.0-x(1))*4.0),
+//                                    0.4) : 0.0;
+//    switch (problem)
+//    {
+//       case 0:
+//          v(0) =  sin(M_PI*x(0)) * cos(M_PI*x(1));
+//          v(1) = -cos(M_PI*x(0)) * sin(M_PI*x(1));
+//          if (x.Size() == 3)
+//          {
+//             v(0) *= cos(M_PI*x(2));
+//             v(1) *= cos(M_PI*x(2));
+//             v(2) = 0.0;
+//          }
+//          break;
+//       case 1: v = 0.0; break;
+//       case 2: v = 0.0; break;
+//       case 3: v = 0.0; break;
+//       case 4:
+//       {
+//          v = 0.0;
+//          const double r = rad(x(0), x(1));
+//          if (r < 0.2)
+//          {
+//             v(0) =  5.0 * x(1);
+//             v(1) = -5.0 * x(0);
+//          }
+//          else if (r < 0.4)
+//          {
+//             v(0) =  2.0 * x(1) / r - 5.0 * x(1);
+//             v(1) = -2.0 * x(0) / r + 5.0 * x(0);
+//          }
+//          else { }
+//          break;
+//       }
+//       case 5:
+//       {
+//          v = 0.0;
+//          if (x(0) >= 0.5 && x(1) >= 0.5) { v(0)=0.0*atn, v(1)=0.0*atn; return;}
+//          if (x(0) <  0.5 && x(1) >= 0.5) { v(0)=0.7276*atn, v(1)=0.0*atn; return;}
+//          if (x(0) <  0.5 && x(1) <  0.5) { v(0)=0.0*atn, v(1)=0.0*atn; return;}
+//          if (x(0) >= 0.5 && x(1) <  0.5) { v(0)=0.0*atn, v(1)=0.7276*atn; return; }
+//          MFEM_ABORT("Error in problem 5!");
+//          return;
+//       }
+//       case 6:
+//       {
+//          v = 0.0;
+//          if (x(0) >= 0.5 && x(1) >= 0.5) { v(0)=+0.75*atn, v(1)=-0.5*atn; return;}
+//          if (x(0) <  0.5 && x(1) >= 0.5) { v(0)=+0.75*atn, v(1)=+0.5*atn; return;}
+//          if (x(0) <  0.5 && x(1) <  0.5) { v(0)=-0.75*atn, v(1)=+0.5*atn; return;}
+//          if (x(0) >= 0.5 && x(1) <  0.5) { v(0)=-0.75*atn, v(1)=-0.5*atn; return;}
+//          MFEM_ABORT("Error in problem 6!");
+//          return;
+//       }
+//       case 7:
+//       {
+//          v = 0.0;
+//          v(1) = 0.02 * exp(-2*M_PI*x(1)*x(1)) * cos(2*M_PI*x(0));
+//          break;
+//       }
+//       default: MFEM_ABORT("Bad number given for problem id!");
+//    }
+// }
 
-double e0(const Vector &x)
-{
-   switch (problem)
-   {
-      case 0:
-      {
-         const double denom = 2.0 / 3.0;  // (5/3 - 1) * density.
-         double val;
-         if (x.Size() == 2)
-         {
-            val = 1.0 + (cos(2*M_PI*x(0)) + cos(2*M_PI*x(1))) / 4.0;
-         }
-         else
-         {
-            val = 100.0 + ((cos(2*M_PI*x(2)) + 2) *
-                           (cos(2*M_PI*x(0)) + cos(2*M_PI*x(1))) - 2) / 16.0;
-         }
-         return val/denom;
-      }
-      case 1: return 0.0; // This case in initialized in main().
-      case 2: return (x(0) < 0.5) ? 1.0 / rho0(x) / (gamma_func(x) - 1.0)
-                        : 0.1 / rho0(x) / (gamma_func(x) - 1.0);
-      case 3: return (x(0) > 1.0) ? 0.1 / rho0(x) / (gamma_func(x) - 1.0)
-                        : 1.0 / rho0(x) / (gamma_func(x) - 1.0);
-      case 4:
-      {
-         const double r = rad(x(0), x(1)), rsq = x(0) * x(0) + x(1) * x(1);
-         const double gamma = 5.0 / 3.0;
-         if (r < 0.2)
-         {
-            return (5.0 + 25.0 / 2.0 * rsq) / (gamma - 1.0);
-         }
-         else if (r < 0.4)
-         {
-            const double t1 = 9.0 - 4.0 * log(0.2) + 25.0 / 2.0 * rsq;
-            const double t2 = 20.0 * r - 4.0 * log(r);
-            return (t1 - t2) / (gamma - 1.0);
-         }
-         else { return (3.0 + 4.0 * log(2.0)) / (gamma - 1.0); }
-      }
-      case 5:
-      {
-         const double irg = 1.0 / rho0(x) / (gamma_func(x) - 1.0);
-         if (x(0) >= 0.5 && x(1) >= 0.5) { return 0.4 * irg; }
-         if (x(0) <  0.5 && x(1) >= 0.5) { return 1.0 * irg; }
-         if (x(0) <  0.5 && x(1) <  0.5) { return 1.0 * irg; }
-         if (x(0) >= 0.5 && x(1) <  0.5) { return 1.0 * irg; }
-         MFEM_ABORT("Error in problem 5!");
-         return 0.0;
-      }
-      case 6:
-      {
-         const double irg = 1.0 / rho0(x) / (gamma_func(x) - 1.0);
-         if (x(0) >= 0.5 && x(1) >= 0.5) { return 1.0 * irg; }
-         if (x(0) <  0.5 && x(1) >= 0.5) { return 1.0 * irg; }
-         if (x(0) <  0.5 && x(1) <  0.5) { return 1.0 * irg; }
-         if (x(0) >= 0.5 && x(1) <  0.5) { return 1.0 * irg; }
-         MFEM_ABORT("Error in problem 6!");
-         return 0.0;
-      }
-      case 7:
-      {
-         const double rho = rho0(x), gamma = gamma_func(x);
-         return (6.0 - rho * x(1)) / (gamma - 1.0) / rho;
-      }
-      default: MFEM_ABORT("Bad number given for problem id!"); return 0.0;
-   }
-}
+// double e0(const Vector &x)
+// {
+//    switch (problem)
+//    {
+//       case 0:
+//       {
+//          const double denom = 2.0 / 3.0;  // (5/3 - 1) * density.
+//          double val;
+//          if (x.Size() == 2)
+//          {
+//             val = 1.0 + (cos(2*M_PI*x(0)) + cos(2*M_PI*x(1))) / 4.0;
+//          }
+//          else
+//          {
+//             val = 100.0 + ((cos(2*M_PI*x(2)) + 2) *
+//                            (cos(2*M_PI*x(0)) + cos(2*M_PI*x(1))) - 2) / 16.0;
+//          }
+//          return val/denom;
+//       }
+//       case 1: return 0.0; // This case in initialized in main().
+//       case 2: return (x(0) < 0.5) ? 1.0 / rho0(x) / (gamma_func(x) - 1.0)
+//                         : 0.1 / rho0(x) / (gamma_func(x) - 1.0);
+//       case 3: return (x(0) > 1.0) ? 0.1 / rho0(x) / (gamma_func(x) - 1.0)
+//                         : 1.0 / rho0(x) / (gamma_func(x) - 1.0);
+//       case 4:
+//       {
+//          const double r = rad(x(0), x(1)), rsq = x(0) * x(0) + x(1) * x(1);
+//          const double gamma = 5.0 / 3.0;
+//          if (r < 0.2)
+//          {
+//             return (5.0 + 25.0 / 2.0 * rsq) / (gamma - 1.0);
+//          }
+//          else if (r < 0.4)
+//          {
+//             const double t1 = 9.0 - 4.0 * log(0.2) + 25.0 / 2.0 * rsq;
+//             const double t2 = 20.0 * r - 4.0 * log(r);
+//             return (t1 - t2) / (gamma - 1.0);
+//          }
+//          else { return (3.0 + 4.0 * log(2.0)) / (gamma - 1.0); }
+//       }
+//       case 5:
+//       {
+//          const double irg = 1.0 / rho0(x) / (gamma_func(x) - 1.0);
+//          if (x(0) >= 0.5 && x(1) >= 0.5) { return 0.4 * irg; }
+//          if (x(0) <  0.5 && x(1) >= 0.5) { return 1.0 * irg; }
+//          if (x(0) <  0.5 && x(1) <  0.5) { return 1.0 * irg; }
+//          if (x(0) >= 0.5 && x(1) <  0.5) { return 1.0 * irg; }
+//          MFEM_ABORT("Error in problem 5!");
+//          return 0.0;
+//       }
+//       case 6:
+//       {
+//          const double irg = 1.0 / rho0(x) / (gamma_func(x) - 1.0);
+//          if (x(0) >= 0.5 && x(1) >= 0.5) { return 1.0 * irg; }
+//          if (x(0) <  0.5 && x(1) >= 0.5) { return 1.0 * irg; }
+//          if (x(0) <  0.5 && x(1) <  0.5) { return 1.0 * irg; }
+//          if (x(0) >= 0.5 && x(1) <  0.5) { return 1.0 * irg; }
+//          MFEM_ABORT("Error in problem 6!");
+//          return 0.0;
+//       }
+//       case 7:
+//       {
+//          const double rho = rho0(x), gamma = gamma_func(x);
+//          return (6.0 - rho * x(1)) / (gamma - 1.0) / rho;
+//       }
+//       default: MFEM_ABORT("Bad number given for problem id!"); return 0.0;
+//    }
+// }
 
 
 static void display_banner(std::ostream &os)
